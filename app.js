cd57ffca6b22e4e27808c3d33572649b65e2bee7
Wajahat-ali18 <162779630+Wajahat-ali18@users.noreply.github.com>
2024-03-09 08:13:11 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+// personName = 'wajahat sipio';
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.replace(/\b\w/g, function (char) { return char.toUpperCase(); }));
